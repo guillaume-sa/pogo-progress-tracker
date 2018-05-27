c277bca208bb4b48f78ab34f17688274e992c95e
Guillaume Sousa <gsousa.amaral@gmail.com>
2018-05-26 23:19:51 -0400

    Add model constraint: max xp

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,9 +1,12 @@
+from django.core.validators import MaxValueValidator
 from django.db import models
+
+MAX_XP = 20000000
 
 
 class XpEntry(models.Model):
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    value = models.PositiveIntegerField()
+    value = models.PositiveIntegerField(validators=[MaxValueValidator(MAX_XP)])
     datetime = models.DateTimeField()
 
     class Meta:
