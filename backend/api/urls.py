d39f5b0413da411563ad495c8e06d340a7d43dc6
Guillaume Sousa <gsousa.amaral@gmail.com>
2018-06-04 00:06:19 -0400

    fix: add missing trailing slash

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,9 +5,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
 
 urlpatterns = [
-    url(r'^auth$', auth_token_views.obtain_auth_token, name='auth'),
+    url(r'^auth/$', auth_token_views.obtain_auth_token, name='auth'),
     url(r'^xp/$', views.XpEntryList.as_view()),
-    url(r'^xp/latest$', views.LatestXpEntryDetail.as_view()),
+    url(r'^xp/latest/$', views.LatestXpEntryDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- frontend/src/config/Api.js
+++ frontend/src/config/Api.js
@@ -1,2 +1,2 @@
 export const URL = 'http://127.0.0.1:8000';
-export const AUTH = "/api/auth";
+export const AUTH = "/api/auth/";
